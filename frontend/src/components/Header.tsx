53709a236c4452564749176c2a417baa7674431f
omkar-frontend <omkar.frontenddev@gmail.com>
2026-05-09 01:04:24 +0530

    Update Header component to wrap title in a Link for navigation to the homepage

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
     return (
         <header className="bg-black text-white p-4 border-b border-neutral-800 sticky top-0 z-10 flex items-center justify-between">
-            <p className="text-center text-xl font-semibold">Jobs Board</p>
+            <Link to="/">
+            <p className="text-center text-xl font-semibold">Jobs <span className="text-emerald-500">Board</span></p>
+            </Link>
         </header>
     )
 } 
